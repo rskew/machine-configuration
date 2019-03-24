b1eab55eafb10c17d0d8ad44d6896d4fb090e673
System administrator <root@ro-X1.lan>
2019-03-24 20:49:43 +1100

    Laptop config

---
+++ configuration.nix
@@ -0,0 +1,205 @@
+# Laptop config
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  networking.hostName = "ro-X1";
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "source-code-pro";
+    consoleKeyMap = "us";
+    defaultLocale = "en_AU.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Australia/Melbourne";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    vimHugeX
+    ctags
+    bat
+    emacs
+    feh
+    git
+    tree
+    xbindkeys
+    xcompmgr
+    xlibs.xmodmap
+    xlibs.xev
+    xlibs.xinput
+    xlibs.xmessage
+    fish
+    sqlite
+    rxvt_unicode
+    firefox
+    pandoc
+    i3lock
+    python3
+    trayer
+    networkmanagerapplet
+    vlc
+    xclip
+    haskellPackages.xmobar
+    pavucontrol
+    pinta
+    inkscape
+    zip
+    unzip
+    nmap
+    gnupg
+    bc
+    imagemagick
+    xorg.xbacklight
+    killall
+    jdk
+    qt5.qtbase
+    awscli
+    xorg.xdpyinfo
+    glxinfo
+    pstree
+  ];
+
+  fonts.fonts = with pkgs; [
+    source-code-pro
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  services.printing.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  services.xserver = {
+
+    enable = true;
+    layout = "us";
+    xkbOptions = "eurosign:e";
+
+    # Enable touchpad support.
+    libinput.enable = true;
+
+    desktopManager.xterm.enable = false;
+
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages: [
+        haskellPackages.xmonad-contrib
+        haskellPackages.xmonad-extras
+        haskellPackages.xmonad
+      ];
+    };
+    windowManager.default = "xmonad";
+    # This used to me my .xinitrc
+    displayManager.sessionCommands = with pkgs; lib.mkAfter
+      ''
+      xbindkeys
+      
+      xrdb -merge /home/rowan/.Xresources
+      xmodmap /home/rowan/.Xmodmap
+      
+      xset s off
+      xset -dpms
+      xset s noblank 
+      
+      trayer --edge bottom \
+             --align right \
+             --SetDockType true \
+             --SetPartialStrut true \
+             --expand true \
+             --width 4 \
+             --transparent true \
+             --tint 0x000000 \
+             --height 20 \
+             --monitor "primary" &
+      
+      exec nm-applet &
+      exec redshift &
+      
+      feh --bg-scale ~/Pictures/jupyter_near_north_pole.jpg &
+      xcompmgr -c &
+      
+      emacs --daemon &
+      '';
+  };
+
+  # Not sure if this is needed
+  programs.fish.enable = true;
+
+  users.users.rowan = {
+    isNormalUser = true;
+    home = "/home/rowan";
+    uid = 1000;
+    extraGroups = [ "wheel" "networkmanager" "audio" ];
+    shell = "/run/current-system/sw/bin/fish";
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+  services.redshift = {
+    enable = true;
+    # Melbourne
+    latitude = "-37.8136";
+    longitude = "144.9631";
+  };
+
+  # This X1 won't wake up from sleep
+  services.logind.extraConfig = "HandleLidSwitch=hibernate";
+
+  systemd.services.lockScreenOnWake = { 
+    description = "Lock screen on wakeup";
+    wantedBy = [ "hibernate.target" ];
+    before = [ "hibernate.target" ];
+    path = with pkgs; [ i3lock ];
+    serviceConfig = {
+      Environment = "DISPLAY=:0";
+      User = "rowan";
+    };
+    script = ''
+      /home/rowan/scripts/lock.sh
+    '';
+    serviceConfig.Type = "forking";
+  };
+
+}
