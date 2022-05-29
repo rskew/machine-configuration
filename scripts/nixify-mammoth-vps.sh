22852d4c242348fb7e3459f5af9988b4c7d7781c
Rowan Skewes <rowan.skewes@gmail.com>
2022-05-29 14:40:07 +1000

    Refactor flake.nix

--- scripts/nixify-mammoth-vps.sh
+++ scripts/nixify-mammoth-vps.sh
@@ -3,17 +3,11 @@
 set -euxo pipefail
 
 function usage {
-    echo "Usage: nixify-mammoth-vps.sh name-of-host-in-ssh-config name-of-nixos-configuration-in-flake"
+    echo "Usage: nixify-mammoth-vps.sh name-of-host-in-ssh-config"
 }
 
 SSH_HOST=${1-}
 if [ -z "$SSH_HOST" ]; then
-    usage
-    exit 1
-fi
-
-NIXOS_CONFIGURATION=${2-}
-if [ -z "$NIXOS_CONFIGURATION" ]; then
     usage
     exit 1
 fi
@@ -25,6 +19,17 @@ ssh $SSH_HOST git clone https://github.com/elitak/nixos-infect.git
 ssh $SSH_HOST "cd nixos-infect; PROVIDER=digitalocean ./nixos-infect"
 
 #### To be tested
+
+#function usage {
+#    echo "Usage: nixify-mammoth-vps.sh name-of-host-in-ssh-config name-of-nixos-configuration-in-flake"
+#}
+
+#NIXOS_CONFIGURATION=${2-}
+#if [ -z "$NIXOS_CONFIGURATION" ]; then
+#    usage
+#    exit 1
+#fi
+
 #ssh $SSH_HOST git clone https://github.com/rskew/machine-configuration.git
 #
 #ssh $SSH_HOST nix-shell --cmd "'nixos-rebuild switch --flake /root/machine-configuration#${NIXOS_CONFIGURATION}'"
