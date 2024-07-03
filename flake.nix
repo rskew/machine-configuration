35008cf0331f1d782a7e09815a30775745df87d9
Rowan Skewes <rowan.skewes@gmail.com>
2024-07-03 21:19:46 +1000

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
@@ -933,7 +933,6 @@
               hardware.nvidia.prime.offload.enable = true;
               hardware.opengl = {
                 enable = true;
-                driSupport = true;
                 driSupport32Bit = true;
               };
               virtualisation.docker.enableNvidia = true;
